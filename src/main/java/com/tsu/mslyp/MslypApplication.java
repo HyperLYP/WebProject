c0a176c13e12c3caa9e86fea188007040673eee2
Monica <1379531829@qq.com>
2023-03-06 18:39:41 +0800

    init commit

---
+++ src/main/java/com/tsu/mslyp/MslypApplication.java
@@ -0,0 +1,13 @@
+package com.tsu.mslyp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MslypApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MslypApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tsu/mslyp/MslypApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsu.mslyp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MslypApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
