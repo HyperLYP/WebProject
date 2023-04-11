9fbd4483bce5b87d6f86e451aa61dc1f4802c85f
Monica <1379531829@qq.com>
2023-04-11 08:51:50 +0800

    [sync] 202304110851

--- src/pages/news.vue
+++ src/pages/news.vue
@@ -1,7 +1,10 @@
 <!--  -->
 <template>
     <div class='news'>
-        <van-nav-bar class="nav-bar" title="新闻"/>
+        <van-nav-bar class="nav-bar" title="新闻" />
+        <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+            <van-cell v-for="item in list" :key="item.id" :title="item.title" />
+        </van-list>
     </div>
 </template>
 
@@ -14,14 +17,41 @@ export default {
     data() {
         //这里存放数据
         return {
-
+            list: [],
+            loading: false,
+            finished: false,
         };
     },
     mounted() {
-        // 联网请求
-        this.$api.api_news.getNews().then(resp => {
-            console.log(resp);
-        })
+
+    },
+    methods: {
+        onLoad() {
+            // 联网请求
+            this.$api.api_news.getNews().then(resp => {
+                console.log(resp);
+                this.list = resp.result.newslist;
+                this.list.push(resp.result.newslist.length);
+                // 加载状态结束
+                this.loading = false;
+                this.finished = true;
+            })
+            // 异步更新数据
+            // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+            // setTimeout(() => {
+            //     for (let i = 0; i < 10; i++) {
+            //         this.list.push(this.list.length + 1);
+            //     }
+
+            //     // 加载状态结束
+            //     this.loading = false;
+
+            //     // 数据全部加载完成
+            //     if (this.list.length >= 40) {
+            //         this.finished = true;
+            //     }
+            // }, 1000);
+        },
     }
 }
 </script>
@@ -34,9 +64,11 @@ export default {
     align-items: center; */
     flex-flow: column nowrap;
 }
+
 .news /deep/ .van-nav-bar__title {
     color: white;
 }
+
 .nav-bar {
     background-color: pink;
 }
