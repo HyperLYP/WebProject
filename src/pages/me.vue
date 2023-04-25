a9c535fdc03eb953b0ae5b2369d25d9ea1b511d6
Monica <1379531829@qq.com>
2023-04-25 08:33:11 +0800

    [sync] 202304250833

--- src/pages/me.vue
+++ src/pages/me.vue
@@ -2,6 +2,36 @@
 <template>
     <div class='me'>
         <van-nav-bar class="nav-bar" title="我的" />
+        <div class='container'>
+            <div class="headimg">
+                <van-image cover round width="100px" height="100px" :src="require('@/assets/head_default.png')" />
+            </div>
+            <div class="tag">
+                <van-tag type="primary">健身达人</van-tag>
+                <van-tag type="success">考研小能手</van-tag>
+                <van-tag type="danger">学习ing</van-tag>
+                <van-tag type="warning">编程爱好者</van-tag>
+            </div>
+            <van-divider></van-divider>
+            <ul>
+                <li>
+                    我的收藏
+                </li>
+                <van-divider></van-divider>
+                <li>
+                    浏览历史
+                    <van-divider></van-divider>
+                </li>
+                <li>
+                    清除缓存
+                </li>
+                <van-divider></van-divider>
+                <li>
+                    设置
+                </li>
+                <van-divider></van-divider>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -30,5 +60,33 @@ export default {
 
 .nav-bar {
     background-color: pink;
+}
+
+.container {
+    display: flex;
+    flex-flow: wrap column;
+    align-content: center;
+    background-color: #fff;
+    height: calc(100vh - 96px);
+}
+
+.headimg {
+    margin: 10px;
+    align-self: center;
+}
+
+.tag {
+    display: flex;
+    width: 305px;
+    justify-content: flex-start;
+    flex-flow: row wrap;
+}
+
+.tag .van-tag {
+    margin-left: 8px;
+}
+
+ul li {
+    color: gray;
 }
 </style>
--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
 // 在这里引入你所需的组件
-import { Button, Toast, Cell, Popup, Image as VanImage, Tabbar, TabbarItem, NavBar, NoticeBar, List, PullRefresh, Swipe, SwipeItem, Grid, GridItem } from 'vant';
+import { Button, Toast, Cell, Popup, Image as VanImage, Tabbar, TabbarItem, NavBar, NoticeBar, List, PullRefresh, Swipe, SwipeItem, Grid, GridItem, Divider, Tag } from 'vant';
 // 按需引入UI组件
-Vue.use(Button).use(Toast).use(Cell).use(Popup).use(VanImage).use(Tabbar).use(TabbarItem).use(NavBar).use(NoticeBar).use(List).use(PullRefresh).use(Swipe).use(SwipeItem).use(Grid).use(GridItem);
+Vue.use(Button).use(Toast).use(Cell).use(Popup).use(VanImage).use(Tabbar).use(TabbarItem).use(NavBar).use(NoticeBar).use(List).use(PullRefresh).use(Swipe).use(SwipeItem).use(Grid).use(GridItem).use(Divider).use(Tag);
