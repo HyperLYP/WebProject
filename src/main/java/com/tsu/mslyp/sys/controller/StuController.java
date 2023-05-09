69dc761851bd3d38c9495fbaaafec0081b158869
Monica <1379531829@qq.com>
2023-05-09 09:36:08 +0800

    [sync] 202305090936

--- src/main/java/com/tsu/mslyp/sys/controller/StuController.java
+++ src/main/java/com/tsu/mslyp/sys/controller/StuController.java
@@ -1,7 +1,14 @@
 package com.tsu.mslyp.sys.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.tsu.mslyp.common.ResultEntity;
+import com.tsu.mslyp.sys.entity.Stu;
+import com.tsu.mslyp.sys.service.IStuService;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,7 +19,14 @@ import org.springframework.stereotype.Controller;
  * @since 2023-04-26
  */
 @Controller
+@RestController
 @RequestMapping("/sys/stu")
 public class StuController {
-
+    @Resource
+    private IStuService stuService;
+    @RequestMapping("list")
+    public ResultEntity<List<Stu>> findStuList() {
+        List<Stu> list = stuService.list();
+        return ResultEntity.success(list);
+    }
 }
