aa32de71f463219970ee45998b0803bc8e630eb7
Monica <1379531829@qq.com>
2023-05-16 09:21:22 +0800

    [sync] 202305160921

--- src/api/api_info.js
+++ src/api/api_info.js
@@ -1,12 +1,16 @@
 // home模块的请求接口
 import request from '@/api/request';
 // 首先导入封装好的axios
-const api_home = {
-  xxx() {
+const api_info = {
+  getStuList(currentPage, pageSize) {
     return request({
       method: 'GET',
-      url: '/xxx',
+      url: '/sys/stu/list',
+      params: {
+        pageNum:currentPage,
+        pageSize:pageSize,
+      }
     });
   },
 };
-export default api_home;
+export default api_info;
--- src/components/layout/content/info/Info.vue
+++ src/components/layout/content/info/Info.vue
@@ -13,23 +13,49 @@
             </el-button>
           </div>
           <el-table :data="tableData" height="500">
-            <el-table-column prop="stuNum" label="学号"> </el-table-column>
+            <el-table-column prop="stuNum" label="学号" width="150px"></el-table-column>
             <el-table-column prop="stuName" label="姓名"> </el-table-column>
             <el-table-column prop="stuGender" label="性别"> </el-table-column>
             <el-table-column prop="stuPhone" label="手机号"> </el-table-column>
             <el-table-column prop="stuMajor" label="专业"> </el-table-column>
+
             <el-table-column label="操作">
               <template slot-scope="scope">
-                <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-                <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                <el-button
+                  size="mini"
+                  @click="handleEdit(scope.$index, scope.row)"
+                  >编辑</el-button
+                >
+                <el-button
+                  size="mini"
+                  type="danger"
+                  @click="handleDelete(scope.$index, scope.row)"
+                  >删除</el-button
+                >
               </template>
             </el-table-column>
           </el-table>
+          <div class="block">
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="currentPage"
+              :page-sizes="[10, 20, 30, 40, 50]"
+              :page-size="pageSize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="totalCount"
+            >
+            </el-pagination>
+          </div>
         </div>
       </el-col>
     </el-row>
     <!--弹窗组件： 新增组件-->
-    <ExcelInput ref="excelInput" :dialog-detail-visible.sync="dialogDetailVisible" :dialog-name="dialogName" />
+    <ExcelInput
+      ref="excelInput"
+      :dialog-detail-visible.sync="dialogDetailVisible"
+      :dialog-name="dialogName"
+    />
   </div>
 </template>
 <script>
@@ -38,14 +64,11 @@ export default {
   components: { ExcelInput },
   data() {
     return {
+      currentPage: 1, //当前页码
+      pageSize: 10, //每页显示条数
+      totalCount: 0, //总条数
       searchMsg: "",
-      tableData: [
-        {
-          // date: "2016-05-03",
-          // name: "王小虎",
-          // address: "上海市普陀区金沙江路 1516 弄",
-        },
-      ],
+      tableData: [],
       // 新增组件的操作数据：
       dialogDetailVisible: false,
       dialogName: "选择导入的学生excel表：",
@@ -53,12 +76,27 @@ export default {
   },
   mounted() {
     //请求学生列表数据
-    this.$api.api_info.getStuList().then((resp) => {
-      console.log(resp);
-      this.tableData = resp.data;
-    });
+    this.callStuList();
   },
   methods: {
+    //请求学生列表数据
+    callStuList() {
+      this.$api.api_info.getStuList(this.currentPage,this.pageSize).then((resp) => {
+        console.log(resp);
+        this.tableData = resp.data;
+        this.totalCount=resp.count;
+      });
+    },
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+      this.pageSize=val;
+      this.callStuList();
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+      this.currentPage=val;
+      this.callStuList();
+    },
     // 关闭弹窗
     closeDialog() {
       console.log("在info组件中执行关闭操作");
@@ -69,7 +107,7 @@ export default {
       this.dialogDetailVisible = true;
     },
     // 打开新增学生 信息页面：
-    add() { },
+    add() {},
     handleEdit(index, row) {
       console.log(index, row);
     },
@@ -82,7 +120,6 @@ export default {
 <style lang="less" scoped>
 .top {
   margin: 5px;
-
   .el-button {
     width: 70px;
     height: 30px;
@@ -90,7 +127,6 @@ export default {
     padding: 0;
   }
 }
-
 .el-table {
   width: 99%;
 }
